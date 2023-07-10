d293cc6bacca52f73061cd5b46e01ac01cdb61c1
IlyaZolotuhin <1zolotyhin1@gmail.com>
2023-07-10 19:37:09 +0700

    10.07 commit

--- src/Counter/Counter.tsx
+++ src/Counter/Counter.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { Button } from './Button';
+import React, {useEffect, useState} from 'react';
+import {Button} from './Button';
 import s from './Counter.module.css'
 
 function Counter() {
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState<number | string>(0);
     const [maxValue, setMaxValue] = useState<number>(() => {
         const storedMaxValue = localStorage.getItem('maxValue');
         return storedMaxValue ? JSON.parse(storedMaxValue) : 0;
@@ -13,6 +13,9 @@ function Counter() {
         const storedStartValue = localStorage.getItem('startValue');
         return storedStartValue ? JSON.parse(storedStartValue) : 0;
     });
+    const [isSetButtonDisabled, setIsSetButtonDisabled] = useState(true);
+    const isCountButtonDisabled = count >= maxValue;
+    const isResetButtonDisabled = count === 0;
 
     useEffect(() => {
         localStorage.setItem('maxValue', JSON.stringify(maxValue));
@@ -21,79 +24,75 @@ function Counter() {
 
     const onClickCountButton = () => {
         if (count < maxValue) {
-            setCount(count + 1);
+            setCount(+count + 1);
         }
     };
 
     const onClickResetButton = () => {
-        setCount(startValue ?? 0);//посмотреть задание
+        setCount(startValue ?? 0);
     };
 
     const onChangeMaxValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setIsSetButtonDisabled(false)
+        setCount('enter values and press "set"')
         const newValue = parseInt(event.target.value);
         if (newValue <= startValue) {
-            event.target.classList.add('error');
+            setCount('incorrect value!')
             setMaxValue(newValue);
         } else {
-            event.target.classList.remove('error');
             setMaxValue(newValue);
         }
     };
 
     const onChangeStartValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setIsSetButtonDisabled(false)
+        setCount('enter values and press "set"')
         const newValue = parseInt(event.target.value);
         if (newValue < 0 || newValue >= maxValue) {
-            event.target.classList.add('error');
+            setCount('incorrect value!')
             setStartValue(newValue);
+            setIsSetButtonDisabled(true)
         } else {
-            event.target.classList.remove('error');
             setStartValue(newValue);
         }
     };
 
     const onClickSetButton = () => {
+        setIsSetButtonDisabled(true)
         if (maxValue !== null && startValue !== null) {
             setCount(startValue);
         }
     };
 
-    /*let countMessage = 'Enter values and press "Set"';
-    if (maxValue !== null && startValue !== null) {
-        if (startValue < 0 || startValue >= maxValue) {
-            countMessage = <span className="error">Incorrect value!</span>;
-        } else {
-            countMessage = 'Count';
-        }
-    }*/
-
-    const isSetButtonDisabled = startValue < 0 || maxValue <= startValue;
-    const isCountButtonDisabled = count >= maxValue;
-    const isResetButtonDisabled = count === 0;
-
     return (
         <div className="App">
             <div className={s.settings}>
-                <div>
-                    <span>Max value: </span>
-                    <input type="number"  value={maxValue} onChange={onChangeMaxValue} />
+                <div className={s.inputBlock}>
+                    <div>
+                        <span>Max value: </span>
+                        <input className={maxValue <= startValue ? s.error : ""}
+                               type="number" value={maxValue} onChange={onChangeMaxValue}/>
+                    </div>
+                    <div>
+                        <span>Start value: </span>
+                        <input className={startValue < 0 || startValue >= maxValue ? s.error : ""}
+                               type="number" value={startValue} onChange={onChangeStartValue}/>
+                    </div>
                 </div>
-                <div>
-                    <span>Start value: </span>
-                    <input type="number" value={startValue} onChange={onChangeStartValue} />
-                </div>
-                <Button name="Set" disabled={isSetButtonDisabled}
-                        ButtonCallBack={onClickSetButton} />
+
+                <Button name="Set"
+                        disabled={isSetButtonDisabled||maxValue <= startValue}
+                        ButtonCallBack={onClickSetButton}/>
             </div>
             <div className={s.result}>
-                {/*<div style={{ color: count === (maxValue ?? 5) ? 'red' : 'black' }}>{countMessage}</div>*/}
-                <div>{count}</div>
+                <div style={{color: count === maxValue||count ==="incorrect value!" ? 'red' : 'black'}} className={s.count}>{count}</div>
 
-            <div className="btn">
-                <Button name="Count" disabled={isCountButtonDisabled}
-                        ButtonCallBack={onClickCountButton} />
-                <Button name="Reset" disabled={isResetButtonDisabled}
-                        ButtonCallBack={onClickResetButton} />
-            </div>
+                <div className={s.btn}>
+                    <Button name="Count" disabled={isCountButtonDisabled}
+                            ButtonCallBack={onClickCountButton}/>
+                    <Button name="Reset" disabled={isResetButtonDisabled}
+                            ButtonCallBack={onClickResetButton}/>
+                </div>
             </div>
         </div>
     );
@@ -101,36 +100,3 @@ function Counter() {
 
 export default Counter;
 
-
-/*import React, {useEffect, useState} from 'react';
-import {Button} from "./Button";
-
-function Counter() {
-    let [count, setCount] = useState(0);
-
-    const onClickCountButton = () => {
-        if (count < 5) {
-            setCount(count + 1)
-        }
-    }
-
-    const onClickResetButton = () => {
-        setCount(0);
-    }
-
-    return (
-        <div className="App">
-            <div className="result">
-                <div style={{color: count === 5 ? 'red' : 'black'}}>{count}</div>
-            </div>
-            <div className="btn">
-                <Button name={'Count'} ButtonCallBack={onClickCountButton} count={count}/>
-                <Button name={'Reset'} ButtonCallBack={onClickResetButton} count={count}/>
-            </div>
-        </div>
-    )
-
-}
-
-export default Counter;
-*/
---
+++ src/Counter/Inputs.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+/*
+type InputsPropsType = {
+    name: string
+    ButtonCallBack: () => void
+    disabled: boolean
+}
+
+export const Inputs = (props: InputsPropsType) => {
+    const onChangeMaxValue = () => {
+        props.ButtonCallBack();
+    }
+
+
+
+return (
+
+            <div>
+                <span>Max value: </span>
+                <input
+                    type="number"
+                    value={maxValue}
+                    onChange={onChangeMaxValue}
+                    disabled={isCountButtonDisabled}
+                />
+            </div>
+
+);}
+*/
